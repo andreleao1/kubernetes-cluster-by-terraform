019813f3b4e12792853de33e5de6811a8a9ffdb4
André G. Leão da Silva <54383652+andreleao1@users.noreply.github.com>
2023-01-30 23:32:44 -0300

    network is done

--- network.tf
+++ network.tf
@@ -1,6 +1,5 @@
 resource "aws_vpc" "kubernetes_cluster_vpc" {
   cidr_block = "10.0.0.0/16"
-
   tags = {
     Name = "${var.infrastructure_name}vpc"
   }
@@ -18,4 +17,31 @@ resource "aws_subnet" "kubernetes_cluster_subnet" {
   tags = {
     Name = "${var.infrastructure_name}subnet_${count.index}"
   }
+}
+
+resource "aws_internet_gateway" "kubernetes_cluster_internet_gateway" {
+  vpc_id = aws_vpc.kubernetes_cluster_vpc.id
+
+  tags = {
+    Name = "${var.infrastructure_name}_internet_gateway"
+  }
+}
+
+resource "aws_route_table" "kubernetes_cluster_route_table" {
+  vpc_id = aws_vpc.kubernetes_cluster_vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.kubernetes_cluster_internet_gateway.id
+  }
+
+  tags = {
+    Name = "${var.infrastructure_name}_route_table"
+  }
+}
+
+resource "aws_route_table_association" "kubernetes_cluster_route_table_association" {
+  count          = 2
+  route_table_id = aws_route_table.kubernetes_cluster_route_table.id
+  subnet_id      = aws_subnet.kubernetes_cluster_subnet.*.id[count.index]
 }
